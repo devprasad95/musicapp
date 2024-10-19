19c9f0dc76cf6ae51a6cc2344714dd4c08677e08
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-10-19 16:57:22 +0530

    19-oct-2024

--- client/lib/core/theme/theme.dart
+++ client/lib/core/theme/theme.dart
@@ -2,22 +2,20 @@ import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static OutlineInputBorder _border(Color color) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
   static final darkThemeMode = ThemeData.dark().copyWith(
-      scaffoldBackgroundColor: Pallete.backgroundColor,
-      //for text form field
-      inputDecorationTheme: const InputDecorationTheme(
-        contentPadding: EdgeInsets.all(27),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Pallete.borderColor,
-            width: 3,
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Pallete.gradient2,
-            width: 3,
-          ),
-        ),
-      ));
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+    //for text form field
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(Pallete.borderColor),
+      focusedBorder: _border(Pallete.gradient2),
+    ),
+  );
 }
